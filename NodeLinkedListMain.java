456f40fc0c0cf88a32c6dfe79fac20c84f1449fe
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-10-25 11:30:28 +0530

    Linked List using Java Generics

---
+++ NodeLinkedListMain.java
@@ -0,0 +1,12 @@
+package com.assginment.linkedlistDs;
+
+
+public class NodeLinkedListMain {
+	
+
+public static void main(String[] args) {
+	System.out.println("Data Structure Problems using Java Generics");
+	System.out.println("Create a Simple Linked List ");
+
+}
+}
