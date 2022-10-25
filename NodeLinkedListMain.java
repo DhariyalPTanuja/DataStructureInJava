b33804ba093bd66f46cda7efbd8c4e8224db4f3d
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-10-25 12:07:06 +0530

    Appending the elements in list sequence 56 30 70

--- NodeLinkedListMain.java
+++ NodeLinkedListMain.java
@@ -6,11 +6,11 @@ public class NodeLinkedListMain {
 
 	public static void main(String[] args) {
 		
-		System.out.println("LinkedListUC2");
-		System.out.println("Ability to create Linked List by adding 30 and 56 to 70");
-		Node<Integer> firstNode = new Node<>(70);
+		System.out.println("LinkedListUC3");
+		System.out.println("Ability to create Linked List by adding 30 and 70 to 56");
+		Node<Integer> firstNode = new Node<>(56);
 		Node<Integer> secondNode = new Node<>(30);
-		Node<Integer> thirdNode = new Node<>(56);
+		Node<Integer> thirdNode = new Node<>(70);
 
 		LinkedList<Integer> simpleLinkedList = new LinkedList<>();
 
