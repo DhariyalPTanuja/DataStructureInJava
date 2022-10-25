07bb4e3040403b61c8f5d1f806eb25a84cd10de4
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-10-25 12:37:25 +0530

    delete the last element in the LinkedList

--- NodeLinkedListMain.java
+++ NodeLinkedListMain.java
@@ -4,9 +4,9 @@ public class NodeLinkedListMain {
 
 	public static void main(String[] args) {
 
-		System.out.println("LinkedListUC5");
+		System.out.println("LinkedListUC6");
 
-		System.out.println("Ability to delete the first element in the LinkedList of sequence 56->30->70");
+		System.out.println("Ability to delete the last element in the LinkedList of sequence 56->30->70");
 		Node<Integer> firstNode = new Node<>(56);
 		Node<Integer> secondNode = new Node<>(30);
 		Node<Integer> ThirdNode = new Node<>(70);
@@ -23,9 +23,9 @@ public class NodeLinkedListMain {
 
 		simpleLinkedListIterator.iterateSimpleLinkedList(simpleLinkedList);
 		System.out.println(" ");
-		System.out.println("Remove the Node");
-		simpleLinkedList.remove(firstNode.getData());
-		//simpleLinkedList.remove(56);
+		System.out.println("Remove the Node Last node");
+		simpleLinkedList.remove(ThirdNode.getData());
+		
 		SimpleLinkedListIterator<Integer> simpleLinkedListIteratorRemove = new SimpleLinkedListIterator<>();
 
 		simpleLinkedListIteratorRemove.iterateSimpleLinkedList(simpleLinkedList);
