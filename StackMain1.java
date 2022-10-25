3d8ace67aeb945b2db008b2501990fa056a16129
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-10-25 17:36:42 +0530

    Stack operation

---
+++ StackMain1.java
@@ -0,0 +1,11 @@
+package com.assignment.StackQueueDs;
+
+public class StackMain1 {
+	
+ public static void main(String[] args) {
+	
+	 System.out.println(" Use LinkedList to do the Stack operation ");
+}
+ 
+ 
+}
